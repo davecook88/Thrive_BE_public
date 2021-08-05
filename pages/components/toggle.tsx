6b7780d9d014f92734a6dc5bb23bc2a058e11ae0
krishneup <contact@butwalweb.com>
2021-08-05 23:00:49 +0545

    dark mode checkbox into toggle state

--- pages/components/toggle.tsx
+++ pages/components/toggle.tsx
@@ -3,11 +3,15 @@ import React, { useState } from "react";
 const Toggle = () => {
   const [checked, setChecked] = useState<boolean>(true);
 
+  const ToggleNow = () =>{
+    setChecked(!checked)
+  }
+
   return (
     <>
       <div className="form-control">
         <label className="cursor-pointer label space-x-5">
-          <input type="checkbox" className="toggle toggle-secondary" />
+          <input type="checkbox" defaultChecked={checked} onChange={ToggleNow} className="toggle toggle-secondary" />
           <span className="label-text">Dark Mode</span>
         </label>
       </div>
