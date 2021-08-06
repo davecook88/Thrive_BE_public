b9453e08de9c80bd7375290ec4b4a01d2cf583e6
krishneup <contact@butwalweb.com>
2021-08-06 23:49:09 +0545

    minor change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // purge: [], // use this during development state
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'], // use this during production state
+  purge: [], // use this during development state
+  // purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'], // use this during production state
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
