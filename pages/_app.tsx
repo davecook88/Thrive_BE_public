09254b6e74aa235c7b9d4c6ac3e4eac463e2dc9e
krishneup <contact@butwalweb.com>
2021-08-06 22:59:18 +0545

    page transitions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from './layouts/header/header'
@@ -7,16 +7,23 @@ import { useRouter } from 'next/router'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const [loading,setLoading] = useState<boolean>(true);
+
   useEffect(() => {
     console.log("checkme")
-    // return () => {
-    //   cleanup
-    // }
+    // setLoading(false);
+    setTimeout(function(){ setLoading(false); }, 300);
+
+    return () => {
+      setLoading(true)
+    }
+
   }, [pageProps])
   return (
     <>
     <Header />
-    <div className="animate-pulse">
+    <div className={loading === true ? 'animate-pulse' : ''}>
     <Component {...pageProps} />
     </div>
     
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,33 @@
 module.exports = {
-  // purge: [], // use this during development state
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'], // use this during production state
+  purge: [], // use this during development state
+  // purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'], // use this during production state
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        pulse: 'pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+       },
+       keyframes :{
+         pulse :{
+          '0%, 100% ':{
+            opacity: 0.1,
+          },
+          '50%': {
+            opacity: 0.1,
+          }
+         }
+       }
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+    // require('daisyui'),
   ],
-  daisyui: {
-    styled: true,
-    themes: false,
-    rtl: false,
-  },
+  // daisyui: {
+  //   styled: true,
+  //   themes: false,
+  //   rtl: false,
+  // },
 }
