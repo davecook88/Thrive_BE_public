d6ac77f6f795ee3db6a5f8ff7f8a41cd135d97f4
krishneup <contact@butwalweb.com>
2021-08-10 22:53:22 +0545

    directory changes

--- pages/components/redux/reducers/darkMode.tsx
+++ pages/components/redux/reducers/darkMode.tsx
@@ -3,13 +3,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 
 
-const checkout = typeof window !== 'undefined' ? localStorage.getItem('theme') : null
+const darkmodeStatus = typeof window !== 'undefined' ? localStorage.getItem('theme') : null
 
 
 export const themeMode = createSlice({
   name: "themeMode",
   initialState: {
-    value: checkout,
+    value: darkmodeStatus,
   },
   reducers: {
     changeDarkMode: (state, action) => {
