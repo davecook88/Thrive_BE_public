e368747589a671cf20a29c761ed6e441d3937e37
krishneup <contact@butwalweb.com>
2021-08-11 00:00:26 +0545

    navigation menu

--- src/components/redux/store.ts
+++ src/components/redux/store.ts
@@ -1,17 +1,5 @@
-// import { configureStore } from '@reduxjs/toolkit'
-// // import { darkMode } from './reducers/darkMode';
-// import themeModeReducer from './reducers/darkMode'
-
-// export default configureStore({
-//   reducer: {
-//     themeMode : themeModeReducer
-//   },
-// })
-
-
 import { configureStore } from '@reduxjs/toolkit'
 import themeModeReducer from './reducers/darkMode'
-// ...
 
 export const store = configureStore({
   reducer: {
