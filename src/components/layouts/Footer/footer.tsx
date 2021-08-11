25592b85ae45f5a3597c2e0a9fad261487594370
krishneup <contact@butwalweb.com>
2021-08-11 23:22:14 +0545

    text changes

--- src/components/layouts/Footer/footer.tsx
+++ src/components/layouts/Footer/footer.tsx
@@ -7,11 +7,11 @@ const Footer = () => {
         <div className="container  mx-auto ">
         <div className="flex flex-wrap items-center justify-between py-4 mx-auto gap-4 ">
                 <div className="first">
-                    <p className="text-skin-white">© 2021 GitHub, Inc.</p>
+                    <p className="text-skin-white">© Copyright Free Content, Inc.</p>
                 </div>
 
                 <div className="last float-right text-skin-white">
-                    <p>© 2021 GitHub, Inc.</p>
+                    <p>© 2021 AbcXYZ, Inc.</p>
                 </div>
         </div>
         </div>
