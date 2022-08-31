ec5efeedf4e5e14a322458ff1c74f086f961321c
davecook88 <davecook@hotmail.co.uk>
2022-08-31 17:33:12 -0500

    redesigned reviews

--- src/components/pages/landing/ReviewCarousel/index.tsx
+++ src/components/pages/landing/ReviewCarousel/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { ReviewCarouselProps } from "./types";
 import { reviews as defaultReviews } from "./reviews";
 import ReviewDetails from "./ReviewDetails";
@@ -8,15 +8,30 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 const ReviewCarousel: React.FC<ReviewCarouselProps> = ({
   reviews = defaultReviews,
 }) => {
+  const slicedReviews = useMemo(() => {
+    const sliced = [];
+    const sliceSize = 3;
+    for (let i = 0; i < reviews.length; i += sliceSize) {
+      sliced.push(reviews.slice(i, i + sliceSize));
+    }
+    return sliced;
+  }, [reviews]);
   return (
-    <Carousel autoPlay showStatus={false} infiniteLoop showThumbs={false}>
-      {reviews.map((review) => (
+    <Carousel
+      autoPlay={false}
+      showStatus={false}
+      infiniteLoop
+      showThumbs={false}
+    >
+      {slicedReviews.map((reviews) => (
         <div
-          className="flex justify-center p-8 pb-12"
-          id={`carouse-item-${review.id}`}
-          key={review.id}
+          className="p-2 md:p-8 pb-12 md:flex"
+          id={`carouse-item-${reviews[0].id}`}
+          key={reviews[0].id}
         >
-          <ReviewDetails review={review} />
+          {reviews.map((r) => (
+            <ReviewDetails review={r} key={r.id} />
+          ))}
         </div>
       ))}
     </Carousel>
