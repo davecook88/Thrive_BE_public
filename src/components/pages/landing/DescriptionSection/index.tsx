76a2b0aa2343d37417bfeb35bc49987b77a88ea4
davecook88 <davecook@hotmail.co.uk>
2022-08-31 19:59:35 -0500

    fixed layout

--- src/components/pages/landing/DescriptionSection/index.tsx
+++ src/components/pages/landing/DescriptionSection/index.tsx
@@ -29,42 +29,41 @@ const IconsGridLower = tw.div`
 `;
 const DescriptionSection = () => {
   return (
-    <LandingPageSection className="w-full ">
+    <section className="w-full ">
       <div className="w-full flex justify-center">
         <SectionHeader>Courses provide</SectionHeader>
       </div>
-      <div>
-        <IconsGridUpper>
-          <FeatureIcon
-            icon={faMobileScreen}
-            description="Study on the go. Just a few minutes each day and you won't forget."
-            header="Vocabulary App"
-          />
-          <FeatureIcon
-            icon={faCirclePlay}
-            header="Video lessons"
-            description="Pre-class videos to learn grammar and phrases before class."
-          />
-          <FeatureIcon
-            icon={faPersonChalkboard}
-            header="Live Classes"
-            description="Online lessons designed to get students speaking."
-          />
-        </IconsGridUpper>
-        <IconsGridLower>
-          <FeatureIcon
-            icon={faPenToSquare}
-            description="Materials to help you prepare grammar outside of class."
-            header="Offline Exercises"
-          />
-          <FeatureIcon
-            icon={faPeopleGroup}
-            header="Native Teachers"
-            description="Friendly speakers ready to share their language and culture."
-          />
-        </IconsGridLower>
-      </div>
-    </LandingPageSection>
+
+      <IconsGridUpper>
+        <FeatureIcon
+          icon={faMobileScreen}
+          description="Study on the go. Just a few minutes each day and you won't forget."
+          header="Vocabulary App"
+        />
+        <FeatureIcon
+          icon={faCirclePlay}
+          header="Video lessons"
+          description="Pre-class videos to learn grammar and phrases before class."
+        />
+        <FeatureIcon
+          icon={faPersonChalkboard}
+          header="Live Classes"
+          description="Online lessons designed to get students speaking."
+        />
+      </IconsGridUpper>
+      <IconsGridLower>
+        <FeatureIcon
+          icon={faPenToSquare}
+          description="Materials to help you prepare grammar outside of class."
+          header="Offline Exercises"
+        />
+        <FeatureIcon
+          icon={faPeopleGroup}
+          header="Native Teachers"
+          description="Friendly speakers ready to share their language and culture."
+        />
+      </IconsGridLower>
+    </section>
   );
 };
 
