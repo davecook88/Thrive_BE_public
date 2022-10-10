9317700e03ca9bcdd28b1978d4f41b1c56641c61
davecook88 <davecook@hotmail.co.uk>
2022-10-10 08:11:02 -0500

    fixed broken imports

--- src/pages/admin/level/index.tsx
+++ src/pages/admin/level/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import UnauthorizedAccessPage from "../../../auth/UnauthorizedPage";
-import { selectUser } from "../../../auth/userSlice";
 import LevelForm from "../../../components/admin/level/LevelForm";
 import { useAppSelector } from "../../../components/redux/hooks";
+import { selectUser } from "../../../components/redux/reducers/user/userSlice";
 
 const LevelAdminPage = () => {
   const auth = useAppSelector(selectUser);
   if (!auth.user?.is_teacher) return <UnauthorizedAccessPage />;
   return (
     <div className="artboard-demo">
-      <div className="mb-4 w-full flex justify-center pt-4">
-        <h1 className="font-bold uppercase text-3xl">Create a level</h1>
+      <div className="mb-4 flex w-full justify-center pt-4">
+        <h1 className="text-3xl font-bold uppercase">Create a level</h1>
       </div>
       <LevelForm refresh={() => {}} />
     </div>
